625b0062403d01385d51d1db2900991ba36c0755
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-11-24 09:00:06 +0600

    all output functionality to show js data

---
+++ Js-Beginner/2.print.js
@@ -0,0 +1,28 @@
+console.log("Wellcome to js ");
+alert("Hellow programmer");
+// inside html to show data
+/*
+ <script>
+ document.getElementById("name").innerHTML =5+66;
+
+ document.write(5+6)
+ </script>
+
+*/ 
+document.getElementById("name").innerHTML =5+66;
+
+// document.write 
+
+document.write(5+6)  // not used 
+
+
+// window.alert()
+
+// this page works fast after the page load 
+
+window.print()
+
+// print is works to print current window  
+
+// note : this is used in the event handler 
+
