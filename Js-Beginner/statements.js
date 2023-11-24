a77e0e8c089a2b646597f21a0e7fb8ec5fe6578a
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-11-24 09:08:29 +0600

    js statements

---
+++ Js-Beginner/statements.js
@@ -0,0 +1,16 @@
+// js statements are composed of values operators expressions keywords and comments 
+
+const course = "this is the js beginner course" ; 
+
+// example of statement 
+// always use semicoln 
+
+// block is used for js group  and used in {} braket 
+
+{
+    let a= 5; 
+}
+
+{
+    let a = 10
+}
