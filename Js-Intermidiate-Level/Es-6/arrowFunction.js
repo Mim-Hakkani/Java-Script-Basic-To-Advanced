c2501200bbf826510e1710e0e993a456e20787fd
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-12-10 20:38:48 +0600

    multipy 3 parameter problem is solved

---
+++ Js-Intermidiate-Level/Es-6/arrowFunction.js
@@ -0,0 +1,7 @@
+//  problem 1 : Write an arrow function that will take 3 parameters, will multiply
+// the parameters and will return the result.
+
+ 
+const multiplyParameter = (a,b,c)=>a*b*c;
+const result = multiplyParameter(4,2,3);
+console.log("Result : ", result);
